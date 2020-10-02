37d80082317ad488333c8f96311a69e60736854d
predoli <olivermaier0@gmail.com>
2020-10-02 18:12:06 +0200

    working entry point

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='wordpress-image-prepare',
-    version='0.5',
-    package_dir={'':'src'},
-    py_modules=['main', 'imageRenamer', 'imageResizer', 'metaCleaner'],
+    version='0.51',
+    packages=find_packages(),
     install_requires=['exif>=1.0.1', 'Pillow >= 7.2.0'],
     entry_points={  # Optional
-        'console_scripts': ['wordpress-image-prepare=WordpressImagePrepare.main:main'],
+        'console_scripts': ['wordpress-image-prepare=src.main:main'],
     },)
